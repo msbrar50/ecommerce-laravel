227450ff4d583a7057adf1e20be402cc8345b114
MANPREET SINGH <knick.mapreet@gmail.com>
2025-02-07 14:02:47 +0000

    login with session completed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\ProductController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::view("/","login");
+Route::get('/login',function(){
+    return view('login');
+});
+
+Route::post('/login',[UserController::class,'login']);
+
+Route::get('/',[ProductController::class,'index']);
